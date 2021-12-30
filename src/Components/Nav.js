cb5f91e72e980e3e5b50328c7a60060b0890eeaa
StephenFinegan21 <x20191952@student.ncirl.com>
2021-12-30 19:04:33 +0000

    Adding a Readme file

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -6,7 +6,6 @@ const Nav = () => {
             <div class="w-4/5 mx-auto h-20">
                 <h2 class="pl-4 pt-8 text-offWhite">DevJobs</h2>
             </div>
-            
         </div>
     )
 }
