50869180e238d3fb65f726a54a5ca566c0f8f486
StephenFinegan21 <x20191952@student.ncirl.com>
2022-01-31 22:44:49 +0000

    Using useReducer instead of UseState for updating values in the job filter

--- src/Components/Container.js
+++ src/Components/Container.js
@@ -1,15 +1,46 @@
-import {React, useEffect, useState} from 'react'
+import {React, useEffect, useState, useReducer} from 'react'
 import Job from './Job'
 import Data from '../data/data.json'
 
-
+const jobsReducer = (state, action) => {
+    switch (action.type) {
+        case 'JOBS_FETCH_INIT':
+          return {
+            ...state,
+            isLoading: true,
+            isError: false,
+          };
+        case 'JOBS_FETCH_SUCCESS':
+          return {
+            ...state,
+            isLoading: false,
+            isError: false,
+            data: action.payload,
+          };
+        case 'JOBS_FETCH_FAILURE':
+          return {
+            ...state,
+            isLoading: false,
+            isError: true,
+          };
+       
+        default:
+          throw new Error();
+      }
+    };
 
 
 const Container = ( { filterValue, locationValue, isChecked, isSubmitted }) => {
 
+        /*  Was initially using useState, switched to useReducer to ensure more consistent data
     const [jobsArray , setJobsArray] = useState()  //List that will hold Jobs
     const [isLoading, setIsLoading] = useState(false);
     const [isError, setIsError] = useState(false); //Any error values held here
+    */
+    const [jobsArray, dispatchJobs] = useReducer(
+        jobsReducer,
+        { data: [], isLoading: false, isError: false }
+      );
 
     
 
@@ -23,55 +54,68 @@ const Container = ( { filterValue, locationValue, isChecked, isSubmitted }) => {
 
         
         useEffect(() => {
-            setIsLoading(true);
+            dispatchJobs({ type: 'JOBS_FETCH_INIT' });
             getAsyncJobs().then(result => {
                 
                 //Filtering by Job and Location
                 if(locationValue && filterValue){ 
-                    setJobsArray( 
-                         Data.filter(m => 
+                    dispatchJobs({
+                        type: 'JOBS_FETCH_SUCCESS',
+                        payload: result.data.jobs.filter(m => 
                             m.location.toLowerCase().includes(locationValue.toLowerCase()) && 
                             (m.company.toLowerCase().includes(filterValue.toLowerCase()) || m.position.toLowerCase().includes(filterValue.toLowerCase())) 
-                            ))
-                }
+                            )
+                        });
+                    }
                //Filtering by Location only
                else if(locationValue){
-                setJobsArray(  Data.filter(m => m.location.toLowerCase().includes(locationValue.toLowerCase())) )
-                }
+                dispatchJobs({
+                    type: 'JOBS_FETCH_SUCCESS',
+                    payload:  result.data.jobs.filter(m => m.location.toLowerCase().includes(locationValue.toLowerCase())) 
+                });}
 
                 //Filtering by Job Value only
                 else if(filterValue){
-                    setJobsArray( Data.filter(m => m.company.toLowerCase().includes(filterValue.toLowerCase()) | m.position.toLowerCase().includes(filterValue.toLowerCase()) ) )
+                    
+                    dispatchJobs({
+                        type: 'JOBS_FETCH_SUCCESS',
+                        payload:  result.data.jobs.filter(m => m.company.toLowerCase().includes(filterValue.toLowerCase()) | m.position.toLowerCase().includes(filterValue.toLowerCase())) 
+                    });
                 }
                 //No Filters
                 else{
-                    setJobsArray( Data )
+                    dispatchJobs({
+                        type: 'JOBS_FETCH_SUCCESS',
+                        payload:  result.data.jobs
+                    });
                 }
                 
                 //setNewArray (isChecked ? newArray.filter(job => job.contract === 'Full Time') : newArray  )
                 // Filtering by Contract (Full time vs Part time is now done in JSX below with conditional rendering)
 
-                setIsLoading(false);
-            })
-            .catch(() => setIsError(true));
+              
+            }).catch(() =>
+            dispatchJobs({ type: 'JOBS_FETCH_FAILURE' }));
+    
+            
 
             //Use effect will run whenever one of the dependcies below changes (ie when user interacts with a filter)
           }, [isSubmitted]);        
 
-    
+    //console.log(jobsArray)
         
         
 
     return (
         
         <div className=" w-4/5 mx-auto h-full grid grid-cols-1  gap-x-8 gap-y-12  md:grid-cols-2 lg:grid-cols-3 ">
-          {isError && <p>Something went wrong ...</p> /*Show error if exists */} 
+          {jobsArray.isError && <p>Something went wrong ...</p> /*Show error if exists */} 
             
             {/* Map through the Data taken from JSON file and render a Job component 
              If isChecked is True (Box ticked) - only map through Full time jobs*/}
-            {isLoading ? <p>Loading ...</p> :
+            {jobsArray.isLoading ? <p>Loading ...</p> :
      
-            jobsArray &&  jobsArray.filter(jobItem => 
+            jobsArray &&  jobsArray.data.filter(jobItem => 
                 isChecked ? jobItem.contract === 'Full Time': jobItem)
                     .map((job) => 
                 <Job 
@@ -84,7 +128,7 @@ const Container = ( { filterValue, locationValue, isChecked, isSubmitted }) => {
                     postedAt = {job.postedAt}
                     requirements = {job.requirements}
                     role = {job.role}
-                /> )} 
+            /> )} 
         </div>
     )
 }
